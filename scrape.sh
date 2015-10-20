95bfe8ff9d9e2cd5714f9e0ee8a8d78f2dc0765d
Walt Roloson <waltroloson@users.noreply.github.com>
2015-10-20 08:23:33 -0500

    Update scrape.sh
    Export python settings to adjust every time the script runs.

--- scrape.sh
+++ scrape.sh
@@ -4,6 +4,8 @@
 #
 # This script runs the scraping of Facebook and Twitter
 
+export PYTHONPATH=.
+export SCRAPY_SETTINGS_MODULE=settings
 scrapy runspider scrapeFacebook.py -a input_file=input/facebook.csv -a output_file=output/facebooklikes.csv
 echo
 scrapy runspider scrapeTwitterProfile.py -a input_file=input/twitter.csv -a output_file=output/twitter.csv
